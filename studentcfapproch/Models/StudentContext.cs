41cfa6a94de0fa2b625bb68d3f51c1e96b3dde8b
demoxs <devatjps@gmail.com>
2023-06-13 15:36:54 +0530

    Add project files.

---
+++ studentcfapproch/Models/StudentContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace studentcfapproch.Models
+{
+    public class StudentContext: DbContext
+    {
+        public StudentContext():base("studentdb")
+        {
+
+        }
+        public DbSet<student> Students { get; set; }
+
+    }
+}
---
+++ studentcfapproch/Models/student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace studentcfapproch.Models
+{
+    public class student
+    {
+        public int id { get; set; }
+        public string studentname { get; set; }
+        public string course { get; set; }
+        public string age { get; set; }
+    }
+}
